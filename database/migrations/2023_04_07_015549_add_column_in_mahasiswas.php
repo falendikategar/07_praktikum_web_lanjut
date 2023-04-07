98befdd4f4f8ad079afaa389696f0ff99e89b0e6
Lenka <falendikapratama@gmail.com>
2023-04-07 10:25:34 +0700

    first commit

---
+++ database/migrations/2023_04_07_015549_add_column_in_mahasiswas.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('mahasiswas', function (Blueprint $table) {
+            $table->string('Email', 50)->unique()->after('No_Handphone');
+            $table->date('Tgl_Lahir')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('mahasiswas', function (Blueprint $table) {
+            $table->dropColumn('Email');
+            $table->dropColumn('Tgl_Lahir');
+        });
+    }
+};
---
+++ resources/views/mahasiswas/index.blade.php
@@ -0,0 +1,75 @@
+@extends('mahasiswas.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left mt-2">
+                <h2>JURUSAN TEKNOLOGI INFORMASI-POLITEKNIK NEGERI MALANG</h2>
+            </div>
+            <div class="row">
+                <div class="col-10 ml-2">
+                    <form action="{{ route('search') }}" method="POST">
+                        @csrf
+                        <div class="input-group mb-3">
+                            <input type="text" value="{{ (request()->search) ? request()->search : '' }}"
+                                name="search" class="form-control" placeholder="Cari data yang Anda inginkan!">
+                            <div class="input-group-append">
+                                <button class="btn btn-primary" type="submit">Search</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="col">
+                    <a class="btn btn-success" href="{{ route('mahasiswas.create') }}"> Input Mahasiswa</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <table class="table table-bordered">
+        <tr>
+            <th>Nim</th>
+            <th>Nama</th>
+            <th>Kelas</th>
+            <th>Jurusan</th>
+            <th>No_Handphone</th>
+            <th>Email</th>
+            <th>Tanggal Lahir</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($mahasiswas as $Mahasiswa)
+        <tr>
+
+            <td>{{ $Mahasiswa->Nim }}</td>
+            <td>{{ $Mahasiswa->Nama }}</td>
+            <td>{{ $Mahasiswa->Kelas }}</td>
+            <td>{{ $Mahasiswa->Jurusan }}</td>
+            <td>{{ $Mahasiswa->No_Handphone }}</td>
+            <td>{{ $Mahasiswa->Email }}</td>
+            <td>{{ $Mahasiswa->Tgl_Lahir }}</td>
+            <td>
+                <form action="{{ route('mahasiswas.destroy',$Mahasiswa->Nim) }}" method="POST">
+
+                    <a class="btn btn-info" href="{{ route('mahasiswas.show',$Mahasiswa->Nim) }}">Show</a>
+                    <a class="btn btn-primary" href="{{ route('mahasiswas.edit',$Mahasiswa->Nim) }}">Edit</a>
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+
+    {{ $mahasiswas->links(); }}
+
+@endsection
